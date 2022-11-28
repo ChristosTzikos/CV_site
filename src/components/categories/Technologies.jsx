17b502e07b11cdda4aa7c2ff15d6e13afea1b116
ChristosTzikos <xristostzikos19981@gmail.com>
2022-11-28 15:35:11 +0200

    tailwind

--- src/components/categories/Technologies.jsx
+++ src/components/categories/Technologies.jsx
@@ -10,7 +10,8 @@ import {
     SiAndroidstudio,
     SiVisualstudiocode,
     SiLinux, 
-    SiZorin
+    SiZorin,
+    SiTailwindcss
 
  }
   from 'react-icons/si';
@@ -21,6 +22,7 @@ function Technologies (){
         <div className='flex flex-row gap-x-3 mt-2 mb-2'>
             <IoLogoReact size='42'/>
             <SiAndroidstudio size='40'/>
+            <SiTailwindcss size='40'/>
             <SiVisualstudiocode size='40'/>
             <IoLogoGithub size='40'/>
             <SiLinux size='40'/>
