ef4ae44d565c1d3e39a032e4bf113626a3657ce9
ChristosTzikos <xristostzikos19981@gmail.com>
2022-11-28 18:32:00 +0200

    intro update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ function Intro() {
         className='w-full   flex flex-col items-center justify-center text-center  pt-5  shadow-xl shadow-slate-400 '>
             <h1 className='text-3xl mt-3 md:text-5xl font-bold'>Christos Tzikos</h1>
             <h2 className='text-xl md:text-lg mb-1 font-bold'>Junior Software Developer</h2>
-            <p className='text-lg max-w-xl font-semibold mb-2 '>I am an Automation Engineering student and I am trying to become a full-stack developer.
+            <p className='text-lg max-w-xl font-semibold mb-2 '>I am an Automation Engineering student from Greece and I want to become a full-stack developer.
 I am currently looking for my first job/internship as a software
 engineer. My goal is to hone my coding skills and work with other
 people on inspiring projects.</p>
