ed1d99ba0afbdecc7bfc5153819e960264f17ae2
ChristosTzikos <xristostzikos19981@gmail.com>
2022-11-28 17:59:29 +0200

    Fixed style issues

--- src/components/categories/Skills.jsx
+++ src/components/categories/Skills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Skills(){
 
     return(
-        <div className=' space-y-2 items-center justify-center flex flex-col w-7/12'>
+        <div className=' space-y-2 items-center justify-center flex flex-col w-7/12 max-w-xl'>
 
         
         
